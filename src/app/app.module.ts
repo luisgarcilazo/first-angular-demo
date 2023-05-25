b5ffc32c41808ea7d0e5a6221f5edfb6e82a4115
luisgarcilazo <luis_david_garcilazo_medrano@hotmail.com>
2023-05-25 09:08:41 -0500

    nav, form, and footer seem to look great

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { FormComponent } from './form/form.component';
+import { footer, navbar } from './demo.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    navbar,
+    FormComponent,
+    footer
   ],
   imports: [
     BrowserModule
---
+++ src/app/demo.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component (
+    {
+        selector: 'nav-bar',
+        templateUrl: './nav.component.html'
+    }
+)
+export class navbar {
+    name="Luis";
+}
+@Component (
+    {
+        selector : 'footer',
+        templateUrl: './footer.component.html'
+    }
+)
+export class footer {
+    name="Luis";
+}
